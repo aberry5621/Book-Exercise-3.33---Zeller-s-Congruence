7020c713d6101a107d7011bd382dee7cd16e0d0b
alexberry721 <alexberry721@gmail.com>
2016-09-07 13:35:36 -0700

    Initial Commit

---
+++ Book Exercise 3.33 - Zeller's Congruence/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Book Exercise 3.33 - Zeller's Congruence
+//
+//  Created by ax on 9/7/16.
+//  Copyright © 2016 COMP130. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
